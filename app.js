a91185bd419503edfa45005ad816cfb220db733a
Mariel Pacada <49132861+marielpacada@users.noreply.github.com>
2021-02-21 16:00:51 -0800

    figuring out pug

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+global.fetch = require("node-fetch");
+const app = express();
+app.set("view engine", "pug");
+
+async function getQuote(response) {
+    const url = "https://api.kanye.rest/";
+    const data = await (await fetch(url)).json();
+    console.log(data);
+    response.render("index", { quote: data.quote });
+}
+
+app.get("/", function (req, res) {
+    getQuote(res).catch(error => {
+        console.error(error);
+    });
+});
+
+
+app.listen(3000, function () {
+    console.log("listening on port 3000...")
+});
