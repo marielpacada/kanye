bbcfa02d836628df07203a92a538dd8a027850fc
Mariel Pacada <49132861+marielpacada@users.noreply.github.com>
2021-02-21 20:51:33 -0800

    done lol

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
+const { response } = require("express");
 const express = require("express");
 global.fetch = require("node-fetch");
 const app = express();
 app.set("view engine", "pug");
 
-async function getQuote(response) {
-    const url = "https://api.kanye.rest/";
-    const data = await (await fetch(url)).json();
-    response.render("index", { quote: data.quote });
+async function APIControl(response) { 
+    // getting kanye quote
+    const kanye_url = "https://api.kanye.rest/";
+    const kanye_data = await (await fetch(kanye_url)).json();
+
+    // getting background image
+    const image_url = "https://api.unsplash.com//photos/random?client_id=7Ufxy23MqWfJC6thniMUB5LpqxIMFS3Ly1zhdGdRrkI&orientation=landscape";
+    const image_data = await (await fetch(image_url)).json();
+    const image_src = image_data.urls.full;
+
+    response.render("index", {
+        quote: kanye_data.quote,
+        src: image_src
+    });
 }
 
 app.get("/", function (req, res) {
-    getQuote(res).catch(error => {
+    APIControl(res).catch(error => {
         console.error(error);
     });
 });
 
-
 app.listen(3000, function () {
     console.log("listening on port 3000...")
 });
+
+
+
+
+
+
+
+
+// fetch(URL)
+//   .then(res=>{return res.blob()})
+//   .then(blob=>{
+//     var img = URL.createObjectURL(blob);
+//     // Do whatever with the img
+//     document.getElementById('img').setAttribute('src', img);
+//   })
